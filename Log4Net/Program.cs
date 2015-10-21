a3dd31d9151ea187471886abef02d0605e90a6aa
paulmac2015 <paul.mcmillon@softeknology.com>
2015-10-20 19:11:45 -0500

    Initial Commit

---
+++ Log4Net/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The application has started!");
+            Console.WriteLine("========================================================================");
+            Console.WriteLine(Log4NetTestClass.CalculateValue(100, 0));
+            Console.ReadLine();
+        }
+    }
+
+    static class Log4NetTestClass
+    {
+        //private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        static public decimal CalculateValue(int val1, int val2)
+        {
+            try
+            {
+                return val1 / val2;
+            }
+            catch (Exception ex)
+            {
+                //logger.Error(ex, "An error has occurred!..." + ex.Message);
+                return 0;
+            }
+        }
+    }
+    }
---
+++ Nlog/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NLog;
+
+namespace Nlog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The application has started!");
+            Console.WriteLine("========================================================================");
+            Console.WriteLine(NlogTestClass.CalculateValue(100, 0));            
+            Console.ReadLine();
+        }
+    }
+
+    static class NlogTestClass
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        static public decimal CalculateValue(int val1, int val2)
+        {
+            try
+            {
+                return val1 / val2;
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "An error has occurred!..." + ex.Message);
+                return 0;
+            }
+        }
+    }
+}
