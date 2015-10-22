a379fe181ac5464d4618bc1d756969731f1d8306
paulmac2015 <paul.mcmillon@softeknology.com>
2015-10-22 08:01:56 -0500

    Modified code to  instruct the Log4Net assemblyan to use an external configuration.

--- Log4Net/Program.cs
+++ Log4Net/Program.cs
@@ -1,37 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Log4NetTests;
+using log4net;
+using log4net.Config;
 
+// Configure log4net using the .log4net file
+// This will cause log4net to look for a configuration file
+// called TestApp.exe.log4net in the application base
+// directory (i.e. the directory containing TestApp.exe)
+// The config file will be watched for changes.
+[assembly: XmlConfigurator(ConfigFileExtension = "log4net", Watch = true)]
 namespace Log4Net
-{
-    class Program
+{ 
+    public class Program
     {
+        // Define a static logger variable so that it references the
+        // Logger instance named "MyApp".
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
         static void Main(string[] args)
         {
-            Console.WriteLine("The application has started!");
-            Console.WriteLine("========================================================================");
-            Console.WriteLine(Log4NetTestClass.CalculateValue(100, 0));
-            Console.ReadLine();
+            // Set up a simple configuration that logs on the console...code has beed commented out to allow Log4Net to use a
+            // configuration file set with the "assembly" directive listed above.
+            // BasicConfigurator.Configure();
+
+            log.Info("Entering application.");
+            Log4NetTestClass Log4NetTestClass = new Log4NetTestClass();
+            Log4NetTestClass.CalculateValue(100, 0);
+            log.Info("Exiting application.");
+            Console.ReadKey();
         }
     }
+}
 
-    static class Log4NetTestClass
-    {
-        //private static Logger logger = LogManager.GetCurrentClassLogger();
 
-        static public decimal CalculateValue(int val1, int val2)
-        {
-            try
-            {
-                return val1 / val2;
-            }
-            catch (Exception ex)
-            {
-                //logger.Error(ex, "An error has occurred!..." + ex.Message);
-                return 0;
-            }
-        }
-    }
-    }
