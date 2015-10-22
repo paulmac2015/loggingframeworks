f30e076d79c3cd5e73a708e95f397b65205e1c48
paulmac2015 <paul.mcmillon@softeknology.com>
2015-10-22 08:05:14 -0500

    Added class Log4NetTestClass for testing the Log4Net assembly.

---
+++ Log4Net/Log4NetTestClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+// Import log4net classes.  This allows the class to use the configuration
+using log4net;
+
+namespace Log4NetTests
+{
+    public class Log4NetTestClass
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Log4NetTestClass));
+
+        public decimal CalculateValue(int Val1, int Val2)
+        {
+            log.Info("Simple log entry!");
+
+            try
+            {
+                return Val1 / Val2;
+            }
+            catch (Exception ex)
+            {
+                log.Debug("An error has occurred!", ex);
+                return 0;
+            }
+        }
+    }
+}
