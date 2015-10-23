63fb55a3783d5a5f209fd8e0e032ac60878d8882
paulmac2015 <paul.mcmillon@softeknology.com>
2015-10-23 10:35:29 -0500

    Added EventLog listener

---
+++ EnterpriseLibrary/EntLibLoggingTestClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnterpriseLibrary;
+
+namespace EnterpriseLibrary
+{
+    public class EntLibLoggingTestClass
+    {
+        public static decimal CalculateValue(int Val1, int Val2)
+        {
+            try
+            {
+                return Val1 / Val2;
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+    }
+}
--- EnterpriseLibrary/Program.cs
+++ EnterpriseLibrary/Program.cs
@@ -1,15 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.Practices.EnterpriseLibrary.Logging;
+using Microsoft.Practices.EnterpriseLibrary.Logging.TraceListeners;
 
 namespace EnterpriseLibrary
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static LogWriter logWriter { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
         {
+            try
+            {
+                logWriter = ConfigureEntLibLogging.ConfigureLogWriter();
+
+                if (logWriter.IsLoggingEnabled())
+                {
+                    using (logWriter)
+                    {
+                        logWriter.Write("Enterprise Library Logging test app has been started!");
+                    }
+                }
+
+                //EntLibLogTest logTest = new EntLibLogTest();
+                //logTest.PrintLogEvent();
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                return;
+            }
+            finally
+            {
+                logWriter.Dispose();
+            }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static class ConfigureEntLibLogging
+        {
+            public static LogWriter ConfigureLogWriter()
+            {
+                //var textFileFormatter = new TextFormatter();
+                var flatFileListener = new RollingFlatFileTraceListener(@"C:\Temp\EntLibLgoFile.log");
+                var asynFlatFileTraceListener = new AsynchronousTraceListenerWrapper(flatFileListener, true);
+
+                EventLog ev = new EventLog("Application", ".", "EntLibLogging");
+                EventLog eventLog = new EventLog("Application");
+                var eventLogListener = new EventLogTraceListener(ev);
+                var asyncEventLogListener = new AsynchronousTraceListenerWrapper(eventLogListener, true);
+
+                var loggingConfig = new LoggingConfiguration();
+                loggingConfig.AddLogSource("EntLibFlatFileLogging", SourceLevels.All, false, asynFlatFileTraceListener);
+                loggingConfig.AddLogSource("EntLibEventLogLogging", SourceLevels.All, false, asyncEventLogListener);
+                logWriter = new LogWriter(loggingConfig);
+                return logWriter;
+            }
         }
     }
 }
